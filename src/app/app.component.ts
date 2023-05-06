a24fe2abfbce7c511ec7c8030ab528309c7c312d
EC2 Default User <ec2-user@ip-10-200-0-95.eu-north-1.compute.internal>
2023-05-06 05:02:18 +0000

    added code

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello World</h1>
+    <h3>Automatically built and deployed after each code update <h3>
+    <br>
+    <br>
+    <h5 style= "color: purple; align:right">----pipeline created by rohit----</h5>
+  `
+})
+export class AppComponent {
+
+}
